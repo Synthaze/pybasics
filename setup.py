b3a48472f97b65bea4f2c3a1cca9204df4c985c2
Florian Malard <synthase@synthase-lap.synthase-lap>
2022-02-02 15:01:40 +0100

    None

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from setuptools import setup
+import os
 
-with open('requirements.txt') as f:
+
+with open(os.getcwd() + '/requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(install_requires=required)
